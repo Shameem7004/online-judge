a27d335f78894cdb312d29bac53648eb3151673e
Shameem <shameemrahmani7@gmail.com>
2025-08-26 13:33:33 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -23,8 +23,8 @@ const PORT = process.env.PORT || 8080;
 
 // middleware
 const allowedOrigins = [
-    'https://www.codeversee.in/', // FIX: Add your new custom domain
-    'https://algo-codeverse.vercel.app/',
+    'https://www.codeversee.in', // FIX: Add your new custom domain
+    'https://algo-codeverse.vercel.app',
     process.env.FRONTEND_URL || 'http://localhost:5173'
 ];
 
